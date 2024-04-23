23f9249e44593b2c699725465c55d1adb6273d22
TanhimMalik <tanhimmalik321@gmail.com>
2024-04-23 11:00:01 -0400

    Added classes to index.js

--- index.js
+++ index.js
@@ -1,20 +1,73 @@
 // Import Classes Here
+class Product {
+    constructor(name, price, description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.inStock = true;
+    }
+
+    display() {
+        return `Name: ${this.name}, Price: $${this.price}, Description: ${this.description}`;
+    }
+}
 
 
+class Cart {
+    constructor() {
+        this.products = [];
+        this.total = 0;
+    }
+
+    addProduct(product) {
+        this.products.push(product);
+        this.total += product.price;
+    }
+
+    removeProduct(i) {
+        if (i >= 0 && i < this.products.length) {
+            this.total -= this.products[i].price;
+            this.products.splice(i, 1);
+        }
+    }
+}
 
 
+class Customer {
+    constructor(name, email, shippingAddress) {
+        this.name = name;
+        this.email = email;
+        this.shippingAddress = shippingAddress;
+        this.orderHistory = [];
+    }
+
+    addToOrderHistory(cart) {
+        this.orderHistory.push(cart);
+    }
+}
 
 
+class Auth {
+    constructor() {
+        this.customers = [];
+    }
 
+    register(name, email, shippingAddress) {
+        const customer = new Customer(name, email, shippingAddress);
+        this.customers.push(customer);
+    }
 
-
+    login(email) {
+        return this.customers.find(customer => customer.email === email) || null;
+    }
+}
 
 // DO NOT EDIT BELOW THIS LINE
 try {
     module.exports = {
         Product,
     }
-} catch(e){
+} catch (e) {
 
 }
 
@@ -23,7 +76,7 @@ try {
         Product,
         Cart
     }
-} catch(e){
+} catch (e) {
 
 }
 
@@ -33,7 +86,7 @@ try {
         Cart,
         Customer
     }
-} catch(e){
+} catch (e) {
 
 }
 
@@ -44,6 +97,6 @@ try {
         Customer,
         Auth
     }
-} catch(e){
+} catch (e) {
 
 }
