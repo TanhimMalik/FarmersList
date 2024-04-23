df9d548bc76efd9aa58c2bb44030e82ba7eec410
TanhimMalik <tanhimmalik321@gmail.com>
2024-04-23 11:16:02 -0400

    Updated files correctly to their corresponding files

---
+++ classes/Cart.js
@@ -0,0 +1,20 @@
+class Cart {
+    constructor() {
+        this.products = [];
+        this.total = 0;
+    }
+
+    addProduct(product) {
+        this.products.push(product);
+        this.total += product.price;
+    }
+
+    removeProduct(i) {
+        if (i < this.products.length && i >= 0) {
+            this.total -= this.products[i].price;
+            this.products.splice(i, 1);
+        }
+    }
+}
+
+module.exports = Cart;
--- index.js
+++ index.js
@@ -1,66 +1,11 @@
 // Import Classes Here
-class Product {
-    constructor(name, price, description) {
-        this.name = name;
-        this.price = price;
-        this.description = description;
-        this.inStock = true;
-    }
-
-    display() {
-        return `Name: ${this.name}, Price: $${this.price}, Description: ${this.description}`;
-    }
-}
+const Product = require('./classes/Product');
+const Cart = require('./classes/Cart');
+const Customer = require('./classes/Customer');
+const Auth = require('./classes/Auth');
 
 
-class Cart {
-    constructor() {
-        this.products = [];
-        this.total = 0;
-    }
-
-    addProduct(product) {
-        this.products.push(product);
-        this.total += product.price;
-    }
-
-    removeProduct(i) {
-        if (i >= 0 && i < this.products.length) {
-            this.total -= this.products[i].price;
-            this.products.splice(i, 1);
-        }
-    }
-}
-
-
-class Customer {
-    constructor(name, email, shippingAddress) {
-        this.name = name;
-        this.email = email;
-        this.shippingAddress = shippingAddress;
-        this.orderHistory = [];
-    }
-
-    addToOrderHistory(cart) {
-        this.orderHistory.push(cart);
-    }
-}
-
-
-class Auth {
-    constructor() {
-        this.customers = [];
-    }
-
-    register(name, email, shippingAddress) {
-        const customer = new Customer(name, email, shippingAddress);
-        this.customers.push(customer);
-    }
-
-    login(email) {
-        return this.customers.find(customer => customer.email === email) || null;
-    }
-}
+// Your additional code for index.js goes here
 
 // DO NOT EDIT BELOW THIS LINE
 try {
